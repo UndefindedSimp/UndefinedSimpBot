b129781da2a85bf464a02969314ff5e386d13c15
Amashi <81893450+AmashiSenpai@users.noreply.github.com>
2022-05-07 20:36:23 -0700

    testing the updates channel

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 
-servers = [955443605672190003]
 owners = [
     635959963134459904,
     720991106048720916,
     395899902867275787,
     559226493553737740
 ]
-
-slash_is_global = True
-
-if slash_is_global is True:
-    servers=None
