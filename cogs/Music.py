9c92310c7922e4d988065cf1bb1ba3fc2e4f1e56
Amashi <81893450+AmashiSenpai@users.noreply.github.com>
2022-05-07 22:14:17 -0700

    improving the music stuff a lot

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,132 +1,238 @@
-import collections
-import youtube_dl
+
+import itertools
+
+from numpy import tile
+from sqlalchemy import desc
+from classes.Music import MusicPlayer, Save, YoutubeSource
+
 import discord
-from random import shuffle, choice
-from collections import namedtuple
-import py_youtube as yti
 from discord.ext import commands
-import discord
 
-# class MyLogger(object):
-#     def debug(self, msg):
-#         print(msg)
-
-#     def warning(self, msg):
-#         print(msg)
-
-#     def error(self, msg):
-#         print(msg)
-
-# def my_hook(d):
-#     print(d)
-#     if d['status'] == 'finished':
-#         print('Done downloading, now converting ...')
+class MusicSave:
+    players: dict[int, MusicPlayer] = {}
 
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
-        self.connections = []
+        Save.embed_color = self.bot.color
 
-        self.FFMPEG_OPTIONS = {
-            "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-            "options": '-vn'
-        }
+    @property
+    def players(self):
+        return MusicSave.players
 
-        self.YDL_OPTIONS = {
-            "format": "bestaudio",
-            # "logger": MyLogger(),
-            # "progress_hooks": [my_hook]
-        }
 
-    async def search_yt(self, query: str, limit=15):
-        yt = yti.Search(query, limit)
-        return yt.videos()
+    async def cleanup(self, guild: discord.Guild):
+        try:
+            await guild.voice_client.disconnect()
+        except AttributeError:
+            pass
 
-    @commands.group(
-        name="music"
-    )
-    @commands.guild_only()
-    async def music(self, ctx: commands.Context):
-        pass
+        try:
+            del self.players[guild.id]
+        except KeyError:
+            pass
 
-    @music.command()
+    async def get_player(self, ctx: commands.Context):
+        try:
+            player = self.players.get(ctx.guild.id, None)
+        except KeyError:
+            player = MusicPlayer(ctx)
+            self.players[ctx.guild.id] = player
+        if player == None:
+            player = MusicPlayer(ctx)
+            self.players[ctx.guild.id] = player
+
+        return player
+    
+    async def create_source(self, ctx: commands.Context, query: str):
+        return await YoutubeSource.create_source(ctx, query,
+            loop=self.bot.loop,
+            download=False
+        )
+
+    @commands.command()
     @commands.guild_only()
     async def join(self, ctx: commands.Context):
-        if ctx.author.voice is None:
-            await ctx.send("you need to be in a voice channel")
-        else:
-            vc = ctx.author.voice.channel
-            if ctx.voice_client is None:
-                await vc.connect()
-            else:
-                await ctx.voice_client.move_to(vc)
-
-    @music.command(
-        name="leave"
-    )
-    @commands.guild_only()
-    async def leave(self, ctx: commands.Context):
-        await ctx.voice_client.disconnect()
-
-    @music.command(
-        name="play"
-    )
-    @commands.cooldown(1, 5)
-    @commands.guild_only()
-    async def play(self, ctx: commands.Context, *, query: str):
-
-        if ctx.voice_client is None:
-            user_vc = ctx.author.voice.channel
-            if user_vc is None:
-                await ctx.send("You have to be in a vc to use this command")
-            else:
-                await user_vc.connect()
+        if not ctx.author.voice.channel:
+            await ctx.send("your not in a vc")
+            return
+        channel: discord.VoiceChannel = ctx.author.voice.channel
 
         vc: discord.VoiceClient = ctx.voice_client
 
-        if "http" in query:
-            url = query
-            video = None
+        if vc:
+            await vc.move_to(channel)
         else:
-            video = await self.search_yt(query, limit=1)
-            video = video[0]
-            url = f"https://www.youtube.com/watch?v={video['id']}"
+            await channel.connect(timeout=120, reconnect=True)
 
-        with youtube_dl.YoutubeDL(self.YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **self.FFMPEG_OPTIONS)
+        await ctx.message.add_reaction('✅')
 
-            embed = discord.Embed(
-                color=self.bot.color
-            )
-            if video:
-                embed.title = video['title']
-            else:
-                embed.title = url
-
-            if video:
-                if len(video['thumb']) > 0:
-                    embed.set_image(url=video['thumb'][0])
-
-            try:
-                vc.play(source, after = lambda e: print(e) )
-            except Exception as err:
-                print(err)
-            await ctx.send(embed=embed)
-
-    @music.command()
+    @commands.command()
     @commands.guild_only()
+    async def play(self, ctx: commands.Context, *, query: str):
+        vc = ctx.voice_client
+        channel: discord.VoiceChannel = ctx.author.voice.channel
+
+        if not vc:
+            if not channel:
+                await ctx.send("your not in a vc")
+            await channel.connect(timeout=120, reconnect=True)
+        
+        if vc:
+            if vc.channel.id != channel.id:
+                await vc.move_to(channel)
+
+
+        if not vc:
+            await ctx.send("not connected to voice")
+            return
+        
+        player = await self.get_player(ctx)
+
+        source = await self.create_source(ctx, query)
+
+        await player.queue.put(source)
+
+    @commands.command()
+    @commands.guild_only()
+    async def playing(self, ctx: commands.Context):
+        player = await self.get_player(ctx)
+
+        if not player:
+            return await ctx.send("no songs playing right now")
+
+        await ctx.send(
+            embed=player.np.embeds[0]
+        )
+
+    @commands.command()
+    async def wipequeue(self, ctx: commands.Context):
+        player = await self.get_player(ctx)
+        if player.queue.empty:
+            await ctx.send("queue is already empty")
+        else:
+            for i in range(player.queue.qsize):
+                await player.queue.get()
+
+    @commands.command()
+    @commands.guild_only()
+    async def queue(self, ctx: commands.Context):
+        vc: discord.VoiceClient = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            await ctx.send('I am not currently connected to voice!')
+            return
+
+        player = await self.get_player(ctx)
+        if player.queue.empty():
+            return await ctx.send('queue is empty')
+
+        # Grab up to 5 entries from the queue...
+        upcoming = list(itertools.islice(player.queue._queue, 0, 5))
+
+        fmt = '\n'.join(f'**`{_["title"]}`**' for _ in upcoming)
+        embed = discord.Embed(title=f'Upcoming - Next {len(upcoming)}', description=fmt, color=Save.embed_color)
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def leave(self, ctx: commands.Context):
+        vc: discord.VoiceClient = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            await ctx.send('I am not currently connected to voice!')
+            return
+        
+        await vc.disconnect()
+
+    @commands.command()
     async def pause(self, ctx: commands.Context):
-        await ctx.voice_client.pause()
-        await ctx.send("Paused!")
+        vc = ctx.voice_client
 
-    @music.command()
-    @commands.guild_only()
+        if not vc or not vc.is_playing():
+            await ctx.send("Not currently playing anything")
+            return
+        elif vc.is_paused():
+            return
+        
+        vc.pause()
+        await ctx.send(
+            embed=discord.Embed(
+                title="Paused",
+                description=f"{ctx.author} has paused the song."
+            )
+        )
+
+    @commands.command()
     async def resume(self, ctx: commands.Context):
-        await ctx.voice_client.resume()
-        await ctx.send("resumed")
+        vc: discord.VoiceClient = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send('I am not currently playing anything!', delete_after=20)
+        elif not vc.is_paused():
+            return
+
+        vc.resume()
+        await ctx.send(f'**`{ctx.author}`**: Resumed the song!')
+
+    @commands.command()
+    async def stop(self, ctx: commands.Context):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send('I am not currently playing anything!', delete_after=20)
+
+        await self.cleanup(ctx.guild)
+
+    @commands.command()
+    async def skip(self, ctx: commands.Context):
+        vc: discord.VoiceClient = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send('I am not currently playing anything!')
+
+        if vc.is_paused():
+            pass
+        elif not vc.is_playing():
+            return
+
+        vc.stop()
+        await ctx.send(f'**`{ctx.author}`**: Skipped the song!')
+
+    
+    @commands.command()
+    async def volume(self, ctx: commands.Context, *, vol: float = None):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send('I am not currently connected to voice.')
+
+        if not 0 < vol < 101:
+            return await ctx.send('Please enter a value between 1 and 100.')
+
+        player = await self.get_player(ctx)
+
+        if not vol:
+            return await ctx.send(
+                embed=discord.Embed(
+                    title="Volume",
+                    description=f"{vc.source.volume * 100}%",
+                    color=Save.embed_color
+                )
+            )
+
+        if vc.source:
+            vc.source.volume = vol / 100
+
+        player.volume = vol / 100
+        await ctx.send(
+            embed=discord.Embed(
+                title="Volume Changed",
+                description=f"{ctx.author} has changed the volume to {round(player.volume * 100, 2)}%",
+                color=Save.embed_color
+            )
+        )
 
 
 def setup(bot):
